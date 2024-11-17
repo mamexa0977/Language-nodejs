cd8976b3b84660251522a413a202bac5f42407a8
MOHAMMED ABDULMALIK <119038672+mamexa0977@users.noreply.github.com>
2024-11-17 17:09:18 +0300

    Update teacherschema.js

--- teacher/teacherschema.js
+++ teacher/teacherschema.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// made by mohammed
 const teacherSchema = new mongoose.Schema({
 username: { type: String, required: true },
   password: { type: String, required: true },
